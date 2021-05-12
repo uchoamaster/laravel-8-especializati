e1ad90f20d85d5a6a2ca97a46f19ee7acec4d2ec
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-05-12 16:55:14 -0300

    aplicando laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\{
-    PostController
-};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,14 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::post('/posts',[PostController::class, 'store'])->name('posts.store');
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
